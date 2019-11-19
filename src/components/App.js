61cec5dca78fcf6e0493ab267a238aa7a5adc634
Matheus Santos <matheus.santos@ilegra.com>
2019-11-19 09:31:19 -0300

    Added some studies about ReactJS.

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import PlacarContainer from './PlacarContainer';
+
+export default class App extends React.Component {
+  render() {
+    return <PlacarContainer />;
+  }
+}
---
+++ src/components/BotaoGol.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class BotaoGol extends React.Component {
+  render() {
+    return <button>Gol!</button>;
+  }
+}
