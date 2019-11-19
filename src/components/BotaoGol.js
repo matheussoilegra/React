f363cba74538ffaa919b693f8cc0c542a3543b16
Matheus Santos <matheus.santos@ilegra.com>
2019-11-19 10:06:51 -0300

    Added class about Props.

--- src/components/BotaoGol.js
+++ src/components/BotaoGol.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 export default class BotaoGol extends React.Component {
+  handleClick(event) {
+    event.preventDefault();
+    this.props.marcarGol();
+  }
+
   render() {
-    return <button>Gol!</button>;
+    return <button onClick={this.handleClick.bind(this)}>Gol!</button>;
   }
 }
