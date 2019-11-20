a03c37c0dc128063877932a8cafc3b6e855fd8e2
Matheus Santos <matheus.santos@ilegra.com>
2019-11-20 14:27:17 -0300

    Added code formatter.

--- react-app-routing/src/App.js
+++ react-app-routing/src/App.js
@@ -24,6 +24,6 @@ const Home = () => (
   <div>
     <h1>Home Page</h1>
   </div>
-)
+);
 
 export default App;
--- react-app-routing/src/Shop.js
+++ react-app-routing/src/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch('https://swapi.co/api/films/');
+    const data = await response.json();
+    // console.log(data.results);
+    setData(data.results);
+  };
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {data.map(data => (
+        <li key={data.episode_id}>
+          Episode {data.episode_id} - {data.title}
+        </li>
+      ))}
     </div>
   );
 }
